a858729e287e1bc399edf683c27ac5e5121eeb0f
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2023-11-17 12:45:33 +0900

    Title: 물건 구입 비용, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231117/물건 구입 비용/cost-of-purchase.py
@@ -0,0 +1,23 @@
+n,p = map(int,input().split())
+sales = []
+dp = [int(1e8)]*(n*3+1)
+ans = int(1e8)
+dp[0] = 0
+
+for _ in range(p):
+    num,price = map(int,input().split())
+    sales.append((num,price))
+
+sales.sort()
+
+for i in range(1,n*3+1):
+    for num,price in sales:
+        if num > i:
+            continue
+        else:
+            dp[i] = min(dp[i],dp[i-num]+price)
+        
+for i in range(n,n*3+1):
+    ans = min(ans,dp[i])
+
+print(ans)
